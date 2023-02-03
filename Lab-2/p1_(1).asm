ef0765d8770d64d853b4b64d504af2afddc51d49
Mannmehta04 <mannmehta04@gmail.com>
2023-02-03 21:48:30 +0530

    Lab - 2
    Added Program to fetch data from registers along with memory address
    
    Added p1_(1).asm & p1_(1).txt

--- Lab-2/p1_(1).asm
+++ Lab-2/p1_(1).asm
@@ -5,9 +5,35 @@
 ; a=b 0, zf = 1, cf=0
 ; a<b 2, zf = 0, cf=1
 
+;store in memory:
+lxi h, 2600h
+mvi m, 2Dh
+
+lxi h, 2601h
+mvi m, 14h
+
+lxi h, 2602h
+mvi m, 20h
+
+lxi h, 2603h
+mvi m, 14h
+
+;load from memory
+lxi h, 2600h
+mov b, m
+
+lxi h, 2601h
+mov c, m
+
+lxi h, 2602h
+mov d, m
+
+lxi h, 2603h
+mov e, m
+
 ;load immidiate data
-lxi b, 2D14h
-lxi d, 2014h
+;lxi b, 2D14h
+;lxi d, 2014h
 
 ;compare higher bits
 mov a, b
@@ -32,14 +58,18 @@ jnz greater
 ;store 00 as values are equal
 equal: lxi h, 2500h
 	mvi m, 00h
+;mvi h, 00h
 hlt
 
 ;labels
+
 greater: lxi h, 2500h
 	mvi m, 01h
+;greater: mvi h, 01h
 hlt
 
 smaller: lxi h, 2500h
 	mvi m, 02h
+;smaller: mvi h, 02h
 hlt
 
